e40a42bf7e64c83b9a3d3761d0962283f88b391a
Joan Puigcerver Ibáñez <j.puigcerveribanez@edu.gva.es>
2025-12-01 11:46:33 +0100

    ud4: example

---
+++ ud4/examples/compte_bancari.py
@@ -0,0 +1,32 @@
+class CompteBancari:
+    def __init__(self, titular, saldo=0):
+        self.titular = titular
+        self.__saldo = saldo
+        self.locked = False
+
+    def ingresar(self, quantitat):
+        if quantitat > 0:
+            self.__saldo += quantitat
+
+    def retirar(self, quantitat):
+        if quantitat > 0 and quantitat <= self.__saldo:
+            self.__saldo -= quantitat
+            return True
+        return False
+
+    def __str__(self):
+        return f"CompteBancari(titular={self.titular}, saldo={self.__saldo})"
+
+
+
+if __name__ == "__main__":
+    compte = CompteBancari('Anna', saldo=100)
+    print(compte)
+    compte.ingresar(50)
+    print(compte)
+    retirat = compte.retirar(100)
+    print(f"s'ha retirat 100 euros? {retirat}")
+    print(compte)
+    retirat = compte.retirar(100)
+    print(f"s'ha retirat 100 euros? {retirat}")
+    print(compte)
